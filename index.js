9f1c110f1a0eb84328eea19d01d5f0c66a35a338
nignacio24 <137337046+nignacio24@users.noreply.github.com>
2023-10-05 13:30:27 -0400

    Created About Us, Projects, Services and Contact Us Page
    Created About Us, Project, Services and contact us page and used router.get to navigate pages

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 const express = require("express");
 const app = express();
 const Joi =  require("joi");
+const router = express.Router();
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use('/', router);
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-
+router.get("/", (req, res) => {
     res.render("pages/index");
 });
+
+router.get("/aboutUs", (req, res) => {
+    res.render("pages/aboutus", {
+        title: "About Us",
+        details: [
+            
+        ],
+    });
+});
+
+router.get("/projects", (req, res) => {
+    res.render("pages/projects", {
+        title: "Lists of Projects",
+        details: [
+            
+        ],
+    });
+});
+
+router.get("/services", (req, res) => {
+    res.render("pages/services", {
+        title: "Lists of Services",
+        details: [
+            
+        ],
+    });
+});
+
+router.get("/contactUs", (req, res) => {
+    res.render("pages/contactus", {
+        title: "Contact Us",
+        details: [
+            
+        ],
+    });
+});
+
+
 
 app.listen(5000, ()=>{
     console.log("App is running");
 })
+
